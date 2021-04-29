415b2ba8c72675dcd4506048f5e6043d1b81c686
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-29 11:36:13 +0800

    Set up esbuild to define the value NODE_ENV to production

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -33,8 +33,9 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              import React, {useState} from 'react';
+              import ReactDOM from 'react-dom';
+              console.log(React, ReactDOM, useState);
             `
           };
         }
