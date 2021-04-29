49da7eb23fc1bf325cd4d9fb4050f5e9eea2fdff
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-29 21:36:23 +0800

    Automatic code formatting

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import MonacoEditor, {
   EditorDidMount,
   ChangeHandler
 } from 'react-monaco-editor';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -9,31 +13,51 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const [code, setCode] = useState('');
+
   const onEditorDidMount: EditorDidMount = (editor, monaco) => {
     editor.focus();
   };
 
   const handleChange: ChangeHandler = (newValue, e) => {
+    setCode(newValue);
     onChange(newValue);
   };
 
+  const handleFormat = () => {
+    // format that value
+    const formatted = prettier.format(code, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    });
+    // set the current value back in the editor
+    setCode(formatted);
+  };
+
   return (
-    <MonacoEditor
-      editorDidMount={onEditorDidMount}
-      onChange={handleChange}
-      width='800'
-      height='600'
-      language='javascript'
-      theme='vs-dark'
-      options={{
-        wordWrap: 'on',
-        showUnused: false,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        tabSize: 2
-      }}
-    />
+    <div>
+      <button onClick={handleFormat}>Format</button>
+      <MonacoEditor
+        value={code}
+        editorDidMount={onEditorDidMount}
+        onChange={handleChange}
+        width='800'
+        height='600'
+        language='javascript'
+        theme='vs-dark'
+        options={{
+          wordWrap: 'on',
+          showUnused: false,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2
+        }}
+      />
+    </div>
   );
 };
 
