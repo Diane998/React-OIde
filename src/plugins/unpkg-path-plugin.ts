b638191e95e5d9836a758afa3281224227996a09
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-29 12:04:29 +0800

    Implemented caching layer to reduce the number of requests to unpkg.com

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -40,12 +45,28 @@ export const unpkgPathPlugin = () => {
           };
         }
 
-        const { data, request } = await axios.get(args.path);
-        return {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname
-        };
+        // Check to see if we've already fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        // if it is, return it immediately
+        if (cachedResult) return cachedResult;
+        else {
+          const { data, request } = await axios.get(args.path);
+
+          const result: esbuild.OnLoadResult = {
+            loader: 'jsx',
+            contents: data,
+            resolveDir: new URL('./', request.responseURL).pathname
+          };
+
+          // store reponse in cache
+          await fileCache.setItem(args.path, result);
+
+          return result;
+        }
       });
     }
   };
