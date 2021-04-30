0eda67d2c1a4f7f6b10c1a2dc2e6f704bae29944
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-30 10:04:43 +0800

    Refactored

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,12 @@ import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 import { fetchPlugin } from '../plugins/fetch-plugin';
 import CodeEditor from './CodeEditor';
+import Preview from './Preview';
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -27,8 +28,6 @@ const App = () => {
   const handleClick = async () => {
     if (!ref.current) return;
 
-    iframe.current.srcdoc = html;
-
     const res = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -40,28 +39,8 @@ const App = () => {
       }
     });
 
-    iframe.current.contentWindow.postMessage(res.outputFiles[0].text, '*');
+    setCode(res.outputFiles[0].text);
   };
-
-  const html = `
-  <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (e) => {
-          try {
-            eval(e.data)
-          } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-            console.error(err);
-          }
-        }, false)
-      </script>
-    </body>
-  </html>
-  `;
 
   return (
     <div>
@@ -73,7 +52,7 @@ const App = () => {
       <div>
         <button onClick={handleClick}>Submit</button>
       </div>
-      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='code' />
+      <Preview code={code} />
     </div>
   );
 };
