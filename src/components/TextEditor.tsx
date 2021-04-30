9815d036c4ada94f041d42b2dd5ace9718e9b335
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-30 14:21:12 +0800

    COnditionally toggling the markdown editor

---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && e.target && ref.current.contains(e.target as Node))
+        return;
+
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  const handleClick = () => {
+    setEditing(true);
+  };
+
+  return editing ? (
+    <div ref={ref}>
+      <MDEditor />
+    </div>
+  ) : (
+    <div onClick={handleClick}>
+      <MDEditor.Markdown source={'# Click me to edit'} />
+    </div>
+  );
+};
+
+export default TextEditor;
