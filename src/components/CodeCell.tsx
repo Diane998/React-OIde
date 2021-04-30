a91de43189964ff776fc941e1da84ad86e3421e8
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-30 13:12:48 +0800

    Added debouncing logic

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import bundle from '../budler';
 
 import CodeEditor from './CodeEditor';
@@ -9,23 +9,26 @@ const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onChange = (value: string) => {
-    setInput(value);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
 
-  const handleClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  const handleChange = (value: string) => {
+    setInput(value);
   };
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction='horizontal'>
-          <CodeEditor
-            initialValue='const a = 1;'
-            onChange={value => setInput(value)}
-          />
+          <CodeEditor initialValue='const a = 1;' onChange={handleChange} />
         </Resizable>
         <Preview code={code} />
       </div>
