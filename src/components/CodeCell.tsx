162e42f5d6a1499f0de547bb35ae9fcb20dcadd5
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-09 08:21:50 +0800

    Used redux state

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,18 +1,24 @@
 import { useState, useEffect } from 'react';
 import bundle from '../budler';
+import { useActions } from '../hooks/useActions';
 
 import CodeEditor from './CodeEditor';
 import Preview from './Preview';
 import Resizable from './Resizable';
+import { Cell } from '../redux';
 
-const CodeCell = () => {
-  const [input, setInput] = useState('');
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [bundlingErr, setBundlingErr] = useState('');
   const [code, setCode] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setBundlingErr(output.err);
     }, 1000);
@@ -20,17 +26,17 @@ const CodeCell = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   const handleChange = (value: string) => {
-    setInput(value);
+    updateCell(cell.id, value);
   };
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction='horizontal'>
-          <CodeEditor initialValue='const a = 1;' onChange={handleChange} />
+          <CodeEditor initialValue={cell.content} onChange={handleChange} />
         </Resizable>
         <Preview code={code} bundlingErr={bundlingErr} />
       </div>
